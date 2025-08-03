03a52a207ae47e6e5e44ba57ec12dc1e4b9fa759
Kinyua Wainaina <Kyshman @ Github 42900317+Kyshman@users.noreply.github.com>
2025-08-03 15:03:00 +0300

    sup.fish - don't detach tmux

--- .config/fish/sahara_fish/fish/functions/sup.fish
+++ .config/fish/sahara_fish/fish/functions/sup.fish
@@ -1,9 +1,12 @@
-function sup --wraps='tmux new-session -d -s mysession; sudo apt-fast update; sudo apt-fast -y -f full-upgrade --autoremove; sudo apt-fast autoclean' --description 'alias sup=sudo apt-fast update; sudo apt-fast -y -f full-upgrade --autoremove; sudo apt-fast autoclean'
-    # Create a new tmux session named 'mysession'
-    tmux new-session -d -s mysession
+function sup --wraps='tmux new-session -s mysession; sudo apt-fast update; sudo apt-fast -y -f full-upgrade --autoremove; sudo apt-fast autoclean' --description 'alias sup=sudo apt-fast update; sudo apt-fast -y -f full-upgrade --autoremove; sudo apt-fast autoclean'
+    # Create a new tmux session named 'mysession' and attach to it
+    tmux new-session -s mysession
 
     # Run the commands in the new tmux session
     tmux send-keys -t mysession "sudo apt-fast update" C-m
     tmux send-keys -t mysession "sudo apt-fast -y -f full-upgrade --autoremove" C-m
     tmux send-keys -t mysession "sudo apt-fast autoclean" C-m
+
+    # Attach to the tmux session
+    tmux attach-session -t mysession
 end
