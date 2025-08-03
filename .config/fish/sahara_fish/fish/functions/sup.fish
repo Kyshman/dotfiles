d11743e5a002d5852ab8d9462a34bc7a5d8224da
Kinyua Wainaina <Kyshman @ Github 42900317+Kyshman@users.noreply.github.com>
2025-08-03 14:36:10 +0300

    Added tmux to sup.fish

--- .config/fish/sahara_fish/fish/functions/sup.fish
+++ .config/fish/sahara_fish/fish/functions/sup.fish
@@ -1,5 +1,9 @@
-function sup --wraps='sudo apt-fast update ; sudo apt-fast -y -f full-upgrade --autoremove ;  sudo apt-fast autoclea' --wraps='sudo apt-fast update ; sudo apt-fast -y -f full-upgrade --autoremove ;  sudo apt-fast autoclean' --description 'alias sup=sudo apt-fast update ; sudo apt-fast -y -f full-upgrade --autoremove ;  sudo apt-fast autoclean'
-    sudo apt-fast update
-    sudo apt-fast -y -f full-upgrade --autoremove
-    sudo apt-fast autoclean $argv
+function sup --wraps='tmux new-session -d -s mysession; sudo apt-fast update; sudo apt-fast -y -f full-upgrade --autoremove; sudo apt-fast autoclean' --description 'alias sup=sudo apt-fast update; sudo apt-fast -y -f full-upgrade --autoremove; sudo apt-fast autoclean'
+    # Create a new tmux session named 'mysession'
+    tmux new-session -d -s mysession
+
+    # Run the commands in the new tmux session
+    tmux send-keys -t mysession "sudo apt-fast update" C-m
+    tmux send-keys -t mysession "sudo apt-fast -y -f full-upgrade --autoremove" C-m
+    tmux send-keys -t mysession "sudo apt-fast autoclean" C-m
 end
