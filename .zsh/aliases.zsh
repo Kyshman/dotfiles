216aea4bbd26d0393b825f48cfd631d84fd8ac05
Kyshman <42900317+Kyshman@users.noreply.github.com>
2026-06-19 11:22:47 +0300

    feat(zsh): replace dig with drill for WAN IP aliases and add sslinfo/ytdlp-open functions
    - Switch `wanip` aliases from `dig` to `drill -Q` for consistency and quieter output
    - Add `sslinfo` function to inspect SSL certificate details including SANs, expiry, and days remaining with cross-platform timezone support
    - Add `ytdlp-open` function to download and open YouTube videos using yt-dlp and xdg-open

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -29,9 +29,9 @@ alias -g pbcopy='wl-copy'
 alias -g pbpaste='wl-paste'
 
 # https://unix.stackexchange.com/a/81699/37512
-alias wanip='dig @resolver4.opendns.com myip.opendns.com +short'
-alias wanip4='dig @resolver4.opendns.com myip.opendns.com +short -4'
-alias wanip6='dig @resolver1.ipv6-sandbox.opendns.com AAAA myip.opendns.com +short -6'
+alias wanip='drill -Q @resolver4.opendns.com myip.opendns.com +short'
+alias wanip4='drill -Q @resolver4.opendns.com myip.opendns.com +short -4'
+alias wanip6='drill -Q @resolver1.ipv6-sandbox.opendns.com AAAA myip.opendns.com +short -6'
 
 alias -g open='xdg-open'
 alias moshj='bash $HOME/Documents/Scripts/mosh-with-jump/mosh-with-jump'
